8ba49f8ce76d071d416b8cbc14f7b9da53f1d84e
Rodionsav <rodionsav@mate.academy>
2024-07-22 14:43:54 +0200

    star-wars clone

--- src/reduxApp/store.ts
+++ src/reduxApp/store.ts
@@ -3,14 +3,12 @@ import peopleReducer from "../components/features/peopleSlice";
 import favouriteReducer from "../components/features/favouriteSlice";
 import { David_Libre } from "next/font/google";
 import { handleClientScriptLoad } from "next/script";
-import characterSlice from "@/components/features/characterSlice";
 
 export const store = configureStore({
   reducer: {
     people: peopleReducer,
     favourite: favouriteReducer,
-    character: characterSlice
-  },
+ },
 });
 
 export type AppDispatch = typeof store.dispatch;
